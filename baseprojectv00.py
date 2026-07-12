21a0d741c924a87d292dbbc23b980449f153aec4
hamed-mn <119741563+hamed-mn@users.noreply.github.com>
2026-07-12 11:49:47 +0330

    Update baseprojectv00.py

--- baseprojectv00.py
+++ baseprojectv00.py
@@ -27,6 +27,6 @@ txn_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
 print(f"Transaction successful! Hash: {w3.to_hex(txn_hash)}")
 fkkkiuyuyykjhy67,mm,mnkib hlh hss asdssdasd ad sd adkl,llhhlkhkhkhk
-h;lkksadaslkjlklkjlkjllk,, l  lkjllkj
+h;lkksadaslkjlklkjlkjllk,, l  lkjllkj,mbhklkl
 hghgmjklklkkjkjkjkasd asdkhh kkhkkh hh kmbhha dasdasdasdasd sdas d
 jksad asd asdahds hhkk hsd adsasda sdds dass addsasd asdasd asd
